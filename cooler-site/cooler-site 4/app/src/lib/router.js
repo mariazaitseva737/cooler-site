59861b9643ec3ff592a6018da4984669117dd215
Mary Gill <76392103+mariazaitseva737@users.noreply.github.com>
2026-09-25 23:13:09 +0300

    Delete cooler-site/cooler-site 4 directory

--- cooler-site/cooler-site 4/app/src/lib/router.js
+++
@@ -1,1 +0,0 @@
-export function go(path) { location.hash = '/' + path; window.scrollTo(0, 0); }
--- cooler-site/cooler-site 4/src/pages/robots.txt.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = ({ site }) => {
-  const base = site ? site.href.replace(/\/$/, '') : '';
-  return new Response(
-    `User-agent: *\nAllow: /\nDisallow: /thanks/\nDisallow: /ru/thanks/\n\nSitemap: ${base}/sitemap-index.xml\n`,
-    { headers: { 'Content-Type': 'text/plain; charset=utf-8' } },
-  );
-};
